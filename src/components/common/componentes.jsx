aae0723e6d7a23cf767fd661661662afb5e7c195
Edwin-Guaillas <94380538+Edwin-Guaillas@users.noreply.github.com>
2021-11-17 15:34:46 -0500

    button

--- src/components/common/componentes.jsx
+++ src/components/common/componentes.jsx
@@ -1,10 +1,11 @@
+import { Button } from "@mui/material";
 
 
 export function Componente (){
     return (
      <div>
         <p>Bienvenidos a la App React</p>
-        <button>Iniciar</button>
+        <Button variant="contained">Inicio</Button>
      </div>
      
     );
