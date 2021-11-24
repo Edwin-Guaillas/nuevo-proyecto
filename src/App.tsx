98f30fe20f382d03eeb2763c88e4c002b3ac6f82
Edwin-Guaillas <94380538+Edwin-Guaillas@users.noreply.github.com>
2021-11-24 13:57:26 -0500

    cambios2

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,6 @@ import Componente from './app/pages/Drawer';
 
 
 
-const init = () => {
-  let sessionUser: any = sessionStorage.getItem('user');
-  let user: any;
-  if (!sessionUser) {
-    user = sessionUser;
-  } else {
-    user = JSON.parse(sessionUser);
-  }
-  return user;
-}
-
-
 
 function App() {
   return (
