e2f309fed4cefcfa870f75be4efc109c7b73187e
Edwin-Guaillas <94380538+Edwin-Guaillas@users.noreply.github.com>
2021-12-02 11:03:08 -0500

    Componentess

--- src/app/pages/cards.tsx
+++ src/app/pages/cards.tsx
@@ -1,33 +1,36 @@
 import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import MediaCards from "./MediaCards";
+import './Styles.css';
 
 
-const mediaCards = [{
-    image: "",
-    title: "Album 1",
-    description: '“Ve el mundo. Es más fantástico que cualquier sueño”',
-},
-{
-    image: "",
-    title: "Album 2",
-    description: ' “Hay una especie de magia cuando nos vamos lejos y, al volver, hemos cambiado”',
-},
-{
-    image: "",
-    title: "Album 3",
-    description: '“Viajar te deja sin palabras y después te convierte en un narrador de historias”',
-},
-{
-    image: "",
-    title: "Album 4",
-    description: '“La unión es uno de los ingredientes más importantes de la vida familiar”',
-},
+const mediaCards = [
+    {
+        image: "",
+        title: "Album 1",
+        description: '“Ve el mundo. Es más fantástico que cualquier sueño”',
+    },
+    {
+        image: "",
+        title: "Album 2",
+        description: ' “Hay una especie de magia cuando nos vamos lejos y, al volver, hemos cambiado”',
+    },
+    {
+        image: "",
+        title: "Album 3",
+        description: '“Viajar te deja sin palabras y después te convierte en un narrador de historias”',
+    },
+    {
+        image: "",
+        title: "Album 4",
+        description: '“La unión es uno de los ingredientes más importantes de la vida familiar”',
+    },
+
 ];
 export default function Cards() {
     return (
-        <Box p={6}>
-            <Grid container spacing={5}>
+        <Box p={2} >
+            <Grid container spacing={{ xs: 2, md: 4 }} >
                 {mediaCards.map((mediaCards, i) => {
                     return (
                         <Grid key={i} item>
@@ -36,6 +39,8 @@ export default function Cards() {
                     );
                 })}
             </Grid>
+            <button className="botonAdd"
+            >+</button>
         </Box>
     );
 }
