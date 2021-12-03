cdbb9bd6a5ef89ec8a58ced1499147c5d7ba76e2
Edwin-Guaillas <94380538+Edwin-Guaillas@users.noreply.github.com>
2021-12-02 21:02:57 -0500

    Listas

--- src/app/pages/listas.jsx
+++ src/app/pages/listas.jsx
@@ -4,36 +4,28 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import DraftsIcon from '@mui/icons-material/Drafts';
 import AutoDeleteIcon from '@mui/icons-material/AutoDelete';
-import YouTubeIcon from '@mui/icons-material/YouTube';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import AutoAwesomeMotionIcon from '@mui/icons-material/AutoAwesomeMotion';
+import BedtimeIcon from '@mui/icons-material/Bedtime';
+
 
 const Listas = () => {
     return (
         <div >
             <List component='nav' >
-                <ListItemButton >
-                    <ListItemIcon >
-                        <FacebookIcon color="info" />
-                    </ListItemIcon>
-                    <ListItemText primary='Facebook' />
-                    <ArrowRightIcon />
-                </ListItemButton>
-
                 <ListItemButton>
                     <ListItemIcon>
-                        <YouTubeIcon color="error" />
+                        <BedtimeIcon color='disabled' />
                     </ListItemIcon>
-                    <ListItemText primary='Youtobe' />
+                    <ListItemText primary='Tema' />
                     <ArrowRightIcon />
                 </ListItemButton>
                 <ListItemButton>
                     <ListItemIcon>
-                        <DraftsIcon color="warning" />
+                        <AutoAwesomeMotionIcon />
                     </ListItemIcon>
-                    <ListItemText primary='Correo' />
+                    <ListItemText primary='Recuerdos' />
                     <ArrowRightIcon />
                 </ListItemButton>
                 <Divider />
@@ -44,9 +36,9 @@ const Listas = () => {
                     <ListItemText primary='Papeleria' />
                     <ArrowRightIcon />
                 </ListItemButton>
+
             </List>
         </div>
     )
 }
-
 export default Listas;
