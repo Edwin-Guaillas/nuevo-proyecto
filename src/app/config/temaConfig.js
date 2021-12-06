1c98f5fc67f48f41f9435b37acfb0479ca53e7d9
Edwin-Guaillas <94380538+Edwin-Guaillas@users.noreply.github.com>
2021-12-06 09:49:08 -0500

    Merge branch 'master' of https://github.com/Edwin-Guaillas/nuevo-proyecto into Login

--- src/app/config/temaConfig.js
+++ src/app/config/temaConfig.js
@@ -1,7 +1,7 @@
 import { createTheme } from "@mui/material/styles";
 import { purple } from '@mui/material/colors';
 
-const theme = createTheme({
+const Theme = createTheme({
     palette: {
         primary: {
             main: purple[500]
